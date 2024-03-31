cc3c2c3a75278ae7cdc0125f8f8ea6866a294f34
user4462 <daikhivanthanh@gmail.com>
2024-03-31 20:44:13 +0700

    up-my-blog

---
+++ Jamstack-blog/.eleventy.js
@@ -0,0 +1,14 @@
+module.exports = function(eleventyConfig){
+    eleventyConfig.addPassthroughCopy('./src/style.css');
+    eleventyConfig.addPassthroughCopy('./src/assetsc');
+    // eleventyConfig.setNunjucksEnvironmentOptions({
+	// 	throwOnUndefined: true,
+	// 	autoescape: false, // warning: don’t do this!
+	// });
+    return{
+        dir:{
+            input:"src",
+            output:"public"
+        }
+    };
+}
